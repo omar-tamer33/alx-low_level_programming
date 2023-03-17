21a8b13d76f59dec1fcdf8d9c396874f487b00b4
omar-tamer33 <omartamer5535002@gmail.com>
2023-03-17 14:56:44 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit base ten
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0);
+}
