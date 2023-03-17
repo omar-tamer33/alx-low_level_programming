d71d9f2f98f46b2dcca24289529a5e20c5f5ad45
omar-tamer33 <omartamer5535002@gmail.com>
2023-03-17 16:10:41 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,13 +7,17 @@
  */
 int main(void)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0 ; i < 10 ; i++)
+	while (i < 10)
 	{
 		putchar(i + '0');
-		putchar(',');
-		putchar(' ');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
