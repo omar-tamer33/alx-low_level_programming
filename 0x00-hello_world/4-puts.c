4e177e4e0e24d8b425a016e3ce206faabfc64fda
omar-tamer33 <omartamer5535002@gmail.com>
2023-03-16 17:32:02 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(void)
 {
-puts("with proper grammar, but the outcome is a piece of art,");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
