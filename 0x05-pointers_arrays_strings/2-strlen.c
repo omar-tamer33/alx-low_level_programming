6e5ac6160bc0de9d0fd93e2354cbb2993afb49a2
omar-tamer33 <omartamer5535002@gmail.com>
2023-03-27 17:14:54 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,5 @@
-#include <holberton.h>
-#include <main.h>
+#include <stdio.h>
+#include "main.h"
 
 /**
   * _strlen - Returns the length of a string
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <main.h>
+#include "main.h"
 
 /**
   * puts2 - Prints every other character of a string
