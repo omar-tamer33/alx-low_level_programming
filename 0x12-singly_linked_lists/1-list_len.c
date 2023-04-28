02c06dbbcf6153f3d704c37209d59ebe28b7b7c1
omar-tamer33 <omartamer5535002@gmail.com>
2023-04-28 16:11:03 -0700

    readme

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,19 @@
 #include "lists.h"
 
 /**
- * list_len - finds the number of elements in a linked list
- *@h: pointer to the list
- *
- *Return: the number of elements
-*/
-
+ * list_len - returns the number of elements in a linked list_t list.
+ * @h: pointer to the list.
+ * Return: number of nodes.
+ **/
 size_t list_len(const list_t *h)
+{
+	size_t cont = 0;
+
+	while (h)
 	{
-		size_t elements = 0;
-
-		while (h)
-		{
-			elements++;
-			h = h->next;
-		}
-
-		return (elements);
+		cont++;
+		h = h->next;
 	}
+
+	return (cont);
+}
--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,46 +1,52 @@
 #include "lists.h"
 
-/**
- * add_node_end - Adds a new node at the end of a list
- *@head: pointer to the head of the list
- *@str: string to be added to the list
- *
- *Return: NULL in case of failure
- *or address or the element
-*/
+int _strlen_recursion(char *s);
 
+/**
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: pointer to the first element of the list.
+ * @str: string to set in the new node.
+ * Return: address of the new element, or NULL if it failed
+ **/
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *dup;
-	int len;
-	list_t *new, *last;
+	list_t *new, *aux = *head;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
+	{
 		return (NULL);
-
-	dup = strdup(str);
-	if (str == NULL)
+	}
+	new->str = strdup(str);
+	if (!new->str)
 	{
 		free(new);
 		return (NULL);
 	}
-
-	for (len = 0; str[len];)
-		len++;
-
-	new->str = dup;
-	new->len = len;
+	new->len = _strlen_recursion(new->str);
 	new->next = NULL;
 
-	if (*head == NULL)
-		*head = new;
-	else
+	if (aux)
 	{
-		last = *head;
-		while (last->next != NULL)
-			last = last->next;
-		last->next = new;
+		while (aux->next)
+			aux = aux->next;
+		aux->next = new;
 	}
-	return (*head);
+	else
+		*head = new;
+
+	return (new);
+}
+
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: string.
+ * Return: length of @s.
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == 0)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
